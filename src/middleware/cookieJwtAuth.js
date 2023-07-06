961e4c209881d809729708272c0098c26096da54
ChayFadida <122904042+ChayFadida@users.noreply.github.com>
2023-07-06 22:51:20 +0300

    Develop (#19)
    * need to make api work
    
    * added login and register functionality and docker
    
    * working
    
    * reorginized the directories
    
    * fix some bug
    
    * middle
    
    * stocks is working
    
    * change api
    
    * stock is work with time and kind
    
    * delete days from api, its for premium users only
    
    * RememberMe done
    
    * contactUs
    
    * seach is working
    
    * searching for stocks is working
    
    * save in db the favorites
    
    * selection added
    
    * update
    
    * fixes
    
    * add to favorites working and clean all redundants
    
    * contactUs, login and signUp fixing
    
    * added logout functionality and choose from favorites
    
    * Chay/vercel2 (#14)
    
    added vercel
    
    * Update cool.html (#15)
    
    Co-authored-by: EinanCO <92308515+EinanCO@users.noreply.github.com>
    
    * change jwt pass (#16)
    
    * Chay/jwt (#17)
    
    * change jwt pass
    
    * vercel change
    
    * change jwt
    
    * deleted log.console form some places (#18)
    
    ---------
    
    Co-authored-by: Chay Fadida <chayfadida@Chays-MacBook-Pro.local>
    Co-authored-by: EinanCO <92308515+EinanCO@users.noreply.github.com>
    Co-authored-by: EdenBar01 <edenbar2008@gmail.com>

--- src/middleware/cookieJwtAuth.js
+++ src/middleware/cookieJwtAuth.js
@@ -7,7 +7,7 @@ exports.cookieJwtAuth = (req, res, next) => {
         return res.redirect("/");
     }
     try {
-        const user = jwt.verify(token, "hello_world");
+        const user = jwt.verify(token, process.env.SECRET_JWT);
         req.user = user;
         next();
     } catch (err) {
